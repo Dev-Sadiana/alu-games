e35394f5a6bdcdf6f22860f05758315db2007614
Sadiana da Silva <sadianasilva364@gmail.com>
2024-06-29 22:35:37 -0300

    craindo funções adicionais e comestários explicativos

--- js/app.js
+++ js/app.js
@@ -1,15 +1,36 @@
+let jogosAlugados = 0;
+
+function quantidadeJogosAlugados() {                                    //exibi a quantidade de jogos alugados
+    console.log(`Total de jogos alugados: ${jogosAlugados}`);
+}
+
 function alterarStatus(id) {
-    let gameSelecionado = document.getElementById(`game-${id}`);
+    let gameSelecionado = document.getElementById(`game-${id}`);        //recebe como parâmetro o id relativo ao jogo
     let imagem = gameSelecionado.querySelector('.dashboard__item__img');
     let botao = gameSelecionado.querySelector('.dashboard__item__button');
-
-    if (botao.classList.contains('dashboard__item__button--return')) {
-        botao.classList.remove('dashboard__item__button--return');
-        imagem.classList.remove('dashboard__item__img--rented');
+    let nomeJogo = gameSelecionado.querySelector('.dashboard__item__name');
+    //para devolver o jogo
+    if (botao.classList.contains('dashboard__item__button--return')) {  //verifica se está classe se encontra entre as outras
+        if (confirm(`Você tem certeza que deseja devolver o jogo ${nomeJogo.textContent}?`)) {  //confirma se o usuário quer seguir com ação e devolver o jogo
+        botao.classList.remove('dashboard__item__button--return');      //altera a propriedade de estilo para o botão ficar como 'Alugado'
+        imagem.classList.remove('dashboard__item__img--rented');        //imagem retorna a original
         botao.textContent = `Alugar`;
-    } else {
-        botao.classList.add('dashboard__item__button--return');
-        imagem.classList.add('dashboard__item__img--rented');
+        jogosAlugados--;
+        }
+    //para alugar o jogo
+    } else {                                                         
+        botao.classList.add('dashboard__item__button--return');         //altera a propriedade de estilo para o botão ficar como 'Devolvido'
+        imagem.classList.add('dashboard__item__img--rented');           //imagem fica opaca
         botao.textContent = `Devolver`;
+        jogosAlugados++;
     }
+    //apresenta quantos jogos foram alugados
+    quantidadeJogosAlugados();
+
 }
+
+    // Inicializa a contagem considerando que os jogos já começam alugados
+    document.addEventListener('DOMContentLoaded', function() {
+    jogosAlugados = document.querySelectorAll('.dashboard__item__img--rented').length;
+    quantidadeJogosAlugados();
+});
